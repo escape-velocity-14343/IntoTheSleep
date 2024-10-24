73344f9fecf57df10b7a3550bbd78a14b17a86ab
William W <wangwilli1bsd@gmail.com>
2024-10-24 13:56:18 -0700

    simplified subsystem and command to simple get set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LEDCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LEDCommand.java
@@ -7,105 +7,21 @@ import org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem;
 import kotlin.jvm.functions.Function3;
 
 public class LEDCommand extends CommandBase {
-    LEDSubsystem leds;
-    private Function3<Integer, String, Boolean, Boolean> ledPredicate;
+    LEDSubsystem ledSubsystem;
+    boolean state = false;
 
-    private LEDCommand(LEDSubsystem leds, Function3<Integer, String, Boolean, Boolean> ledPredicate) {
-        this.leds = leds;
-        this.ledPredicate = ledPredicate;
-        addRequirements(this.leds);
+    private LEDCommand(LEDSubsystem leds, boolean enable) {
+        ledSubsystem = leds;
+        state = enable;
     }
 
     @Override
-    public void execute() {
-        leds.setLedsState(ledPredicate);
+    public void initialize(){
+        ledSubsystem.lightSwitch(state);
     }
 
     @Override
     public boolean isFinished() {
         return true;
-    }
-
-    /**
-     * Turns on all LEDs
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @return A new LEDCommand
-     */
-    public static LEDCommand on(LEDSubsystem leds) {
-        return setState(leds, true);
-    }
-
-    /**
-     * Turns on one LED
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param ledName The string name of the LED to set
-     * @return A new LEDCommand
-     */
-    public static LEDCommand on(LEDSubsystem leds, String ledName) {
-        return setState(leds, ledName, true);
-    }
-
-    /**
-     * Turns off all LEDs
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @return A new LEDCommand
-     */
-    public static LEDCommand off(LEDSubsystem leds) {
-        return setState(leds, false);
-    }
-
-    /**
-     * Turns off one LED
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param ledName The string name of the LED to set
-     * @return A new LEDCommand
-     */
-    public static LEDCommand off(LEDSubsystem leds, String ledName) {
-        return setState(leds, ledName, false);
-    }
-
-    /**
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param state The state to set all the LEDs to
-     * @return A new LEDCommand
-     */
-    public static LEDCommand setState(LEDSubsystem leds, boolean state) {
-        return new LEDCommand(leds, (i, key, enabled) -> state);
-    }
-
-    /**
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param ledName The string name of the LED to set
-     * @param state The state to set all the LEDs to
-     * @return A new LEDCommand
-     */
-    public static LEDCommand setState(LEDSubsystem leds, String ledName, boolean state) {
-        return new LEDCommand(leds, (i, key, enabled) -> key == ledName ? state : enabled);
-    }
-
-    /**
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @return A new LEDCommand
-     */
-    public static LEDCommand toggle(LEDSubsystem leds) {
-        return new LEDCommand(leds, (i, key, enabled) -> !enabled);
-    }
-
-    /**
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param ledName The string name of the LED to set
-     * @return A new LEDCommand
-     */
-    public static LEDCommand toggle(LEDSubsystem leds, String ledName) {
-        return new LEDCommand(leds, (i, key, enabled) -> key == ledName ? !enabled : enabled);
-    }
-
-    /**
-     * @param leds The {@link org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem} to use
-     * @param function A function that accepts the index, the name, and the current state of each LED (servo)
-     * @return A new LEDCommand
-     */
-    public static LEDCommand fromFunction(LEDSubsystem leds, Function3<Integer, String, Boolean, Boolean> function) {
-        return new LEDCommand(leds, function);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LEDSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LEDSubsystem.java
@@ -4,45 +4,31 @@ import android.util.ArrayMap;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
 
-import kotlin.jvm.functions.Function3;
-
 public class LEDSubsystem extends SubsystemBase {
-    // uses ArrayMap because HashMap seems overkill for 5 items
-    ArrayMap<String, ServoImplEx> servos;
-
-    public LEDSubsystem(HardwareMap hMap, String... entries) {
-        for (String entry : entries) {
-            servos.put(entry, hMap.get(ServoImplEx.class, entry));
-        }
-    }
-
-    public void setLedState(String servoKey, boolean state) {
-        if (state) {
-            Objects.requireNonNull(servos.get(servoKey)).setPwmEnable();
-        } else {
-            Objects.requireNonNull(servos.get(servoKey)).setPwmDisable();
-        }
+    private ServoImplEx light;
+    public LEDSubsystem(HardwareMap hMap) {
+        light = (ServoImplEx) hMap.get(Servo.class, "light");
+        light.setPosition(0);
+        light.setPwmEnable();
     }
 
     /**
-     * Sets multiple LED states at once, using ledPredicate to decide whether the LED is on or off
-     * @param ledPredicate A function that accepts the index, the name, and the current state of each LED (servo)
+     * True for on, false for off
+     * @param state
      */
-    public void setLedsState(Function3<Integer, String, Boolean, Boolean> ledPredicate) {
-        // I put this method inside LEDSubsystem instead of LEDCommand to maintain strict access
-        // to `servos`
-        int i = 0;
-        for (Map.Entry<String, ServoImplEx> led : servos.entrySet()) {
-            if (ledPredicate.invoke(i++, led.getKey(), led.getValue().isPwmEnabled())) {
-                led.getValue().setPwmEnable();
-            } else {
-                led.getValue().setPwmDisable();
-            }
+    public void lightSwitch(boolean state){
+        if (state) {
+            light.setPwmEnable();
+        }
+        else {
+            light.setPwmDisable();
         }
     }
 }
