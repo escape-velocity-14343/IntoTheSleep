de40c91be34c943c57299e4656dd9cdd17c0ea6e
sockeye-d <106356149+sockeye-d@users.noreply.github.com>
2024-12-09 23:41:30 -0800

    Fixed up a lot of messy code (#19)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/AutoAlignTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/AutoAlignTest.java
@@ -8,38 +8,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Constants.IntakeConstants;
 import org.firstinspires.ftc.teamcode.commands.group.DefaultGoToPointCommand;
-import org.firstinspires.ftc.teamcode.commands.group.SampleAutoAlign;
 import org.firstinspires.ftc.teamcode.commands.group.SampleAutoAlignAndExtend;
 import org.firstinspires.ftc.teamcode.subsystems.CameraSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
 
 @Config
-@Autonomous(name = "Automous Align Testing", group = "Test")
+@Autonomous(name = "Autonomous Align Testing", group = "Test")
 public class AutoAlignTest extends Robot {
     CameraSubsystem cam;
-    private DefaultGoToPointCommand gtpc;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        cam = new CameraSubsystem(hardwareMap, ()->false);
+        cam = new CameraSubsystem(hardwareMap, () -> false);
         initialize();
 
         pinpoint.reset();
         wrist.setWrist(IntakeConstants.groundPos);
         intake.setClawer(IntakeConstants.singleIntakePos);
-        while (!cam.setExposure());
         waitForStart();
         intake.setIntakeSpeed(1);
 
         imu.resetYaw();
         extension.reset();
 
-        gtpc = new DefaultGoToPointCommand(mecanum, pinpoint, new Pose2d(0, 0, new Rotation2d()));
+        DefaultGoToPointCommand gtpc = new DefaultGoToPointCommand(mecanum, pinpoint, new Pose2d(0, 0, new Rotation2d()));
 
         pinpoint.setPosition(0, 0);
 
         cs.schedule(new SequentialCommandGroup(
-            new SampleAutoAlignAndExtend(cam, gtpc, pinpoint, extension)
+                new SampleAutoAlignAndExtend(cam, gtpc, pinpoint, extension)
         ));
 
         cs.schedule(gtpc);
