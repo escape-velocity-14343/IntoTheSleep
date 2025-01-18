2f79d842a584efa70501bedae16ca47b2acddb5a
William H Wang <61762213+cyblazer@users.noreply.github.com>
2025-01-18 07:41:08 -0800

    Partner O7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auton/SubmersibleSample/O7Piece.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auton/SubmersibleSample/O7Piece.java
@@ -7,13 +7,11 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Constants.IntakeConstants;
 import org.firstinspires.ftc.teamcode.Constants.PivotConstants;
 import org.firstinspires.ftc.teamcode.Constants.SlideConstants;
 import org.firstinspires.ftc.teamcode.commands.custom.AutonExtendCommand;
-import org.firstinspires.ftc.teamcode.commands.custom.ConditionalWaitCommand;
 import org.firstinspires.ftc.teamcode.commands.custom.IntakeClawCommand;
 import org.firstinspires.ftc.teamcode.commands.custom.IntakeControlCommand;
 import org.firstinspires.ftc.teamcode.commands.custom.PivotCommand;
@@ -25,19 +23,17 @@ import org.firstinspires.ftc.teamcode.commands.group.DefaultGoToPointCommand;
 import org.firstinspires.ftc.teamcode.commands.group.GoToPointWithDefaultCommand;
 import org.firstinspires.ftc.teamcode.commands.group.IntakePosCommand;
 import org.firstinspires.ftc.teamcode.commands.group.RetractCommand;
-import org.firstinspires.ftc.teamcode.commands.group.TurnToPointWithDefaultCommand;
 import org.firstinspires.ftc.teamcode.subsystems.CameraSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
 
 
 @Config
-@Autonomous(name = "07 Soldier")
+//@Autonomous(name = "6 Sample 0 Specimen Popeyes' 6 Piece")
 public abstract class O7Piece extends Robot {
     private DefaultGoToPointCommand gtpc;
     private CameraSubsystem cam;
 
-    public static Pose2d hpside = new Pose2d(-55, -20, Rotation2d.fromDegrees(-90));
-    public static Pose2d droppedPiece = new Pose2d(-45, 41,new Rotation2d(0));
+    public static Pose2d partnerPos = new Pose2d(-56, 0, Rotation2d.fromDegrees(-90));
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -47,7 +43,7 @@ public abstract class O7Piece extends Robot {
         cam.waitForSetExposure(3000,5000);
 
         pinpoint.reset();
-        wrist.setWrist(IntakeConstants.groundPos);
+        wrist.setWrist(IntakeConstants.foldedPos);
         intake.setClawer(IntakeConstants.closedPos);
 
         waitForStart();
@@ -60,39 +56,31 @@ public abstract class O7Piece extends Robot {
 
 
         cs.schedule(false, new SequentialCommandGroup(
-                new IntakeControlCommand(intake, IntakeConstants.closedPos, IntakeConstants.autoOuttakeSpeed).alongWith(new WaitCommand(300)),
-                new WristCommand(wrist, IntakeConstants.halfFoldPos),
-                new GoToPointWithDefaultCommand(hpside, gtpc).alongWith(
-                        new WristCommand(wrist, IntakeConstants.groundPos).andThen(new IntakePosCommand(extension, pivot, wrist))
-                ).withTimeout(3000),
-                new IntakeControlCommand(intake, IntakeConstants.openPos, 1),
-                new AutonExtendCommand(extension, SlideConstants.autonPiece1Extension).interruptOn(intake::getDSensorSupplier),
-                new IntakeControlCommand(intake, IntakeConstants.closedPos, 1),
-                RetractCommand.newWithWristPos(wrist, pivot, extension, IntakeConstants.halfFoldPos).alongWith(
-                        new GoToPointWithDefaultCommand(scorePos, gtpc)
-                ),
-                new ConditionalWaitCommand(() -> pinpoint.getPose().getY() > hpside.getY() * -1).withTimeout(2500),
-                new BucketPosCommand(extension, pivot, wrist),
-                //new WaitCommand(100),
-                new IntakeControlCommand(intake, IntakeConstants.openPos, -1),
-                new WaitCommand(350),
-                new IntakeControlCommand(intake, IntakeConstants.singleIntakePos, 0),
-                new WristCommand(wrist, IntakeConstants.groundPos), //TODO: I have no clue what we do for retraction
-                new IntakePosCommand(extension, pivot, wrist).alongWith(new TurnToPointWithDefaultCommand(droppedPiece, gtpc)).withTimeout(3000),
-                new IntakeControlCommand(intake, IntakeConstants.openPos, 1),
-                new AutonExtendCommand(extension, SlideConstants.autonPiece1Extension).interruptOn(intake::getDSensorSupplier),
-                new IntakeControlCommand(intake, IntakeConstants.closedPos, 1),
-                // No retract here cuz its pretty close to bucket
+
                 new GoToPointWithDefaultCommand(scorePos, gtpc).alongWith(
                         new BucketPosCommand(extension, pivot, wrist)
                 ),//.withTimeout(3000),
-                new IntakeControlCommand(intake, IntakeConstants.openPos, -1),
+                new IntakeControlCommand(intake,IntakeConstants.openPos, -1),
                 new WaitCommand(250),
                 new IntakeControlCommand(intake, IntakeConstants.singleIntakePos, 0),
                 //new IntakeClawCommand(intake, IntakeConstants.closedPos),
 
+                new GoToPointWithDefaultCommand(partnerPos, gtpc).alongWith(
+                        new WristCommand(wrist, IntakeConstants.halfFoldPos).andThen(new IntakePosCommand(extension, pivot, wrist))
+                ).withTimeout(3000),
+                new WristCommand(wrist, IntakeConstants.groundPos),
+                new IntakeControlCommand(intake, IntakeConstants.openPos, 1),
+                new AutonExtendCommand(extension, SlideConstants.autonPiece1Extension).interruptOn(intake::getDSensorSupplier),
+                new IntakeControlCommand(intake, IntakeConstants.closedPos, 1),
+                //RetractCommand.newWithWristPos(wrist, pivot, extension, IntakeConstants.groundPos)
+                new BucketPosCommand(extension, pivot, wrist).alongWith(
+                        new GoToPointWithDefaultCommand(scorePos, gtpc)
+                ),
+                //new WaitCommand(100),
 
-
+                new IntakeControlCommand(intake,IntakeConstants.openPos, -1),
+                new WaitCommand(350),
+                new IntakeControlCommand(intake, IntakeConstants.singleIntakePos, 0),
 
                 new Auton3Yellows(extension, pivot, wrist, intake, gtpc, cam, pinpoint),
 
